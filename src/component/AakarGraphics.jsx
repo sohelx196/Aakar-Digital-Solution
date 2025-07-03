d7f80eba835e05a4cbe746d2923636ab623d5fd9
sohelx196 <sohel.devloper196@gmail.com>
2025-07-03 15:32:21 +0530

    aakar graphics page completed

--- src/component/AakarGraphics.jsx
+++ src/component/AakarGraphics.jsx
@@ -11,8 +11,9 @@
   import vasantPanchami from '../assets/Images/aakarGraphics/vasantPanchami.jpg'
   import nirmalBike from '../assets/Images/aakarGraphics/nirmalBike.jpg'
   import shivji from '../assets/Images/aakarGraphics/shivji.jpg'
+  import arvindKumar from '../assets/Images/aakarGraphics/arvindKumar.jpg'
 
-
+  import aakarBg from '../assets/Images/otherImages/aakarBg.png'
   const AakarrGraphics = () => {
     useEffect(() => {
       AOS.init({ duration: 1000, once: true });
@@ -21,32 +22,38 @@
     const services = [
       {
         title: "Pulvama Attack",
-        description: "Design for Luxewash",
+        description: "Design for Luxewash in Indore",
         image: pulvamaAttack,
       },
       {
-        title: "Social Media",
-        description: "Eye-catching posts and ads for Facebook, Instagram, and more.",
+        title: "Kadai Panner",
+        description: "Designed for Amrit BLU veg family restaurant, sihada road",
         image: kadaiPartner,
       },
       {
-        title: "Marketing",
-        description: "Billboard and flyer designs optimized for maximum impact.",
+        title: "Vasant Panchami",
+        description: "Designed for Luxewash in Indore",
         image: vasantPanchami,
       },
       {
-        title: "Digital Products",
-        description: "Tailored digital products that fit your branding needs.",
+        title: "Nirmal bike decore",
+        description: "Designed for bike accessories & decore , padawa khandwa",
         image: nirmalBike,
       },
       {
-        title: "Digital Products",
-        description: "Tailored digital products that fit your branding needs.",
+        title: "Marksheet Design",
+        description: "Designed for Arvind Kumar Nitin Kumar School , Khandwa ",
+        image: arvindKumar,
+      },
+      {
+        title: "Shivlok Entry Gate",
+        description: "Entry gate designed for festival pandal",
         image: shivji,
       }
     ];
 
     return (
+      <>
       <section className="flex flex-col bg-white text-gray-800 my-20">
 
         {/*  Hero Section */}
@@ -55,7 +62,7 @@
           className="bg-cover bg-center bg-no-repeat py-20 px-6 text-center relative">
 
           <div className=" p-6 rounded-xl max-w-3xl mx-auto backdrop-blur-md shadow-lg">
-            <h2 className="text-5xl sm:text-5xl lg:text-7xl font-saira font-bold text-white mb-4" data-aos="zoom-in">
+            <h2 className="text-5xl sm:text-5xl lg:text-8xl font-saira font-bold text-white mb-4" data-aos="zoom-in">
               <span className="text-white">AAKAR</span> GRAPHICS
             </h2>
             <p className="font-quicksand text-base sm:text-base lg:text-lg text-slate-200" data-aos="fade-up">
@@ -81,7 +88,7 @@
     </div>
 
     {/* Image */}
-    <div className="md:w-1/2 " data-aos="fade-left">
+    <div className="md:w-1/2 " data-aos="zoom-out">
       <img
         src={graphicImg} // Replace with a different overview image if needed
         alt="Aakar Overview"
@@ -95,10 +102,10 @@
 
 
         {/*  Services Grid */}
-        <section className=" px-4 sm:px-10 lg:px-24 space-y-16">
+        <section className=" px-4 sm:px-10 lg:px-24 space-y-20 lg:space-y-40">
 
-<div data-aos="fade-up" className='h-screen flex justify-center item-center flex-col text-center'>
-  <h1 className='font-saira font-bold text-4xl lg:text-6xl  '>See <span className='text-blue-950 '> Our Work</span></h1>
+<div data-aos="fade-up" className='mt-10 lg:mt-20 flex justify-center item-center flex-col text-center'>
+  <h1 className='font-saira font-bold text-4xl lg:text-6xl  '>See <span className='text-blue-950 '>Our Work</span></h1>
 </div>
 
           {services.map((service, index) => (
@@ -111,10 +118,10 @@
               <img
                 src={service.image}
                 alt={service.title}
-                className="w-full md:w-1/2 border border-yellow-300 rounded-xl shadow-lg  h-64 md:h-80"
+                className="w-full md:w-1/2 border border-yellow-300 rounded-xl shadow-lg object-content h-64 md:h-80"
               />
 
-              <div className="md:w-1/2 ">
+              <div data-aos="zoom-in" className="md:w-1/2 ">
                 <h3 className="font-saira font-extrabold  text-2xl sm:text-3xl lg:text-6xl  mb-1 text-gray-600">{service.title}</h3>
                 <p className="text-blue-950 font-quicksand font-black  text-lg sm:text-lg ">
                   {service.description}
@@ -136,7 +143,21 @@
           </Link>
         </div> */}
 
+
       </section>
+
+ <div
+  data-aos="zoom-out"
+  className="relative border   bg-center bg-cover p-10 rounded-xl text-blue-950 lg:my-28 text-center lg:mx-32 mx-7"
+  style={{ backgroundImage: `url(${aakarBg})` }}>
+
+  <p className="font-quicksand font-semibold text-2xl lg:text-4xl lg:leading-tight leading-relaxed">
+    "We've worked with many amazing people and these projects are just a few of the stories we've created together."
+  </p>
+</div>
+
+
+   </>  
     );
   };
 
